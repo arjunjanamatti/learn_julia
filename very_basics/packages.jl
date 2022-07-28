a8f696fbc7d8c8b90a87884730d16169a103f93e
arjunjanamatti <arjunjanamatti@gmail.com>
2022-07-28 20:35:30 +0530

    finished packages

---
+++ very_basics/packages.jl
@@ -0,0 +1,24 @@
+##### Packages
+using Statistics
+using BenchmarkTools
+
+##### adding Packages
+# add BenchmarkTools
+
+##### removing Packages
+# rm BenchmarkTools
+
+##### adding a specific version
+# add BenchmarkTools@1.2.0 to get the version number 1.2.0
+
+##### Project.toml and Manifest.toml
+# To get manifest, status --manifest [pkgs...]
+# In --manifest mode the output also includes recursive dependencies of added
+# packages listed as arguments the output will be limited to those packages.
+
+
+##### updating Packages
+# up BenchmarkTools
+
+##### clean outdated packages
+# gc = will collect all the packages not used for significant time
