02f63ddefbed1d0430c8844e8fc25c3c9b9e1118
arjunjanamatti <arjunjanamatti@gmail.com>
2022-07-29 23:12:20 +0530

    variables

--- very_basics/03_variables.jl
+++ very_basics/03_variables.jl
@@ -9,3 +9,11 @@ typeof(y)
 ##### Boolean: Bool
 mortality = true
 typeof(mortality)
+
+# Packages, Modules and types are all title case, everything else is lower case
+
+##### Strings: String
+my_string = "Start Julia"
+typeof(my_string)
+
+# Single quote in Julia is reserved only for char types
