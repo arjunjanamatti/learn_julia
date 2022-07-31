77551f69b3b81e687ca5cc1ca5b32d6285331dca
arjunjanamatti <arjunjanamatti@gmail.com>
2022-07-31 16:16:00 +0530

    uploading dataset

--- basic_machine_learning/2_dataframes.jl
+++ basic_machine_learning/2_dataframes.jl
@@ -1,4 +1,5 @@
 using DataFrames
+using CSV
 
 columnTable = (age = [12,15,34],
                height = [1.5,1.9,2.3],
@@ -8,6 +9,12 @@ df_1 = DataFrame(columnTable)
 
 
 # filter the DataFrame
+df_2 = filter(df_1) do row
+    row.married == true
+end
+
 filter(df_1) do row
     row.married == true
 end
+
+titanic_df = DataFrame(CSV.File("train.csv"))
