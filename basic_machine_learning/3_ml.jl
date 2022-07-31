7973b4193cf221f6e368bdce4c499f16df7ca15b
arjunjanamatti <arjunjanamatti@gmail.com>
2022-07-31 21:24:18 +0530

    handling missing values

--- basic_machine_learning/3_ml.jl
+++ basic_machine_learning/3_ml.jl
@@ -21,3 +21,21 @@ describe(titanic_df)
 
 # if we want to change the schema type for a example column
 # coerce(titanic_df, :Sex => Count)
+
+# function to handle missing data in class column
+function class_missing(n)
+    if ismissing(n)
+        return "without cabin"
+    else
+        return "has cabin"
+    end
+end
+
+# ByRow will go row by row like lambda
+titanic_df_1 = DataFrames.transform(titanic_df, :Cabin => ByRow(class_missing) => :Cabin)
+
+# get value counts
+combine(groupby(titanic_df_1, :Cabin), nrow)
+
+size(titanic_df_1)
+
